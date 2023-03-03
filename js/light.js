8372575f7113fddc80745adc1453019fde87566f
faburongqing <3384365840@qq.com>
2023-03-03 23:08:37 +0800

    Site updated: 2023-03-03 23:08:37

---
+++ js/light.js
@@ -0,0 +1,74 @@
+// 防抖全局计时器
+let TT = null;    //time用来控制事件的触发
+// 防抖函数:fn->逻辑 time->防抖时间
+function debounce(fn, time) {
+    if (TT !== null) clearTimeout(TT);
+    TT = setTimeout(fn, time);
+}
+// 复制提醒
+document.addEventListener("copy", function () {
+    debounce(function () {
+        new Vue({
+            data: function () {
+                this.$notify({
+                    title: "哎嘿！复制成功🍬",
+                    message: "若要转载最好保留原文链接哦，给你一个大大的赞！",
+                    position: 'top-left',
+                    offset: 50,
+                    showClose: true,
+                    type: "success",
+                    duration: 5000
+                });
+            }
+        })
+    }, 300);
+})
+// 霓虹灯效果
+// 颜色数组
+var arr = ["#39c5bb", "#f14747", "#f1a247", "#f1ee47", "#b347f1", "#1edbff", "#ed709b", "#5636ed"];
+// 颜色索引
+var idx = 0;
+
+// 切换颜色
+function changeColor() {
+    // 仅夜间模式才启用
+    if (document.getElementsByTagName('html')[0].getAttribute('data-theme') == 'dark') {
+        if (document.getElementById("site-name"))
+            document.getElementById("site-name").style.textShadow = arr[idx] + " 0 0 15px";
+        if (document.getElementById("site-title"))
+            document.getElementById("site-title").style.textShadow = arr[idx] + " 0 0 15px";
+        if (document.getElementById("site-subtitle"))
+            document.getElementById("site-subtitle").style.textShadow = arr[idx] + " 0 0 10px";
+        if (document.getElementById("post-info"))
+            document.getElementById("post-info").style.textShadow = arr[idx] + " 0 0 5px";
+        try {
+            document.getElementsByClassName("author-info__name")[0].style.textShadow = arr[idx] + " 0 0 12px";
+            document.getElementsByClassName("author-info__description")[0].style.textShadow = arr[idx] + " 0 0 12px";
+        } catch {
+            
+        }
+        idx++;
+        if (idx == 8) {
+            idx = 0;
+        }
+    } else {
+        // 白天模式恢复默认
+        if (document.getElementById("site-name"))
+            document.getElementById("site-name").style.textShadow = "#1e1e1ee0 1px 1px 1px";
+        if (document.getElementById("site-title"))
+            document.getElementById("site-title").style.textShadow = "#1e1e1ee0 1px 1px 1px";
+        if (document.getElementById("site-subtitle"))
+            document.getElementById("site-subtitle").style.textShadow = "#1e1e1ee0 1px 1px 1px";
+        if (document.getElementById("post-info"))
+            document.getElementById("post-info").style.textShadow = "#1e1e1ee0 1px 1px 1px";
+        try {
+            document.getElementsByClassName("author-info__name")[0].style.textShadow = "";
+            document.getElementsByClassName("author-info__description")[0].style.textShadow = "";
+        } catch {
+            
+        }
+    }
+}
+
+// 开启计时器
+window.onload = setInterval(changeColor, 1200);
